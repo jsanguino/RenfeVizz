008609d39b6f9c714f4b5c9c8502dcacd6ceae1e
Victoriano <victoriano@gmail.com>
2013-06-28 16:38:20 +0200

    It works but it does not

--- mobiRenfe.rb
+++ mobiRenfe.rb
@@ -11,16 +11,17 @@ session.use "renfe_vizz"
 @driver.manage.timeouts.implicit_wait = 30
 @verification_errors = []
 
-@driver.get(@base_url + "/renfev2/busca_trenes.do;jsessionid=FF8ECCC7F2B53A47187445D06C1AF1A9?ss=FF8ECCC7F2B53A47187445D06C1AF1A9&ga=true")
+@driver.get(@base_url + "/renfev2/busca_trenes.do")
 
 all_optionsO = @driver.find_element(:name, "o").find_elements(:tag_name, "option")
 all_optionsD = @driver.find_element(:name, "d").find_elements(:tag_name, "option")
 
+#Iterate all the combination of Cities
 
 for i in (1).upto(all_optionsO.length)
-  for j in (1).upto(all_optionsO.length)
+  for j in (1).upto(all_optionsD.length)
 
-    @driver.get(@base_url + "/renfev2/busca_trenes.do;jsessionid=FF8ECCC7F2B53A47187445D06C1AF1A9?ss=FF8ECCC7F2B53A47187445D06C1AF1A9&ga=true")
+    @driver.navigate.back
 
     all_optionsO = @driver.find_element(:name, "o").find_elements(:tag_name, "option")
     all_optionsD = @driver.find_element(:name, "d").find_elements(:tag_name, "option")
@@ -41,42 +42,34 @@ for i in (1).upto(all_optionsO.length)
    wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 
    #Journey does exist?
-   if @driver.find_elements(:xpath, ".//*[@id='details']/p/a").first.text == "Buscar Trayecto con transbordo"
+   if @driver.find_elements(:xpath, ".//*[@id='details']/p/a").size()>0
     next
-   else
-   puts "hello world 0"
-
+   end
+  
    #Get All trains for a trip
-   @allTrains
    @trains= Hash.new
-   puts "hello world 1"
 
-   wait.until {
-
-   puts "hello world 2"
    @allTrains = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[*]/li[1]/a")
 
    #parse type of train, schedule, time travel
    for i in (@allTrains.length).downto(1)
-  	@tname = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[1]/a")
-  	puts @tname.first.text
-    @trains[[i,0]]= @tname.first.text
-    @departure = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[2]")
-    puts @departure.first.text.split[1]
-    @trains[[i,1]]= @departure.first.text.split[1]
-    @arrival = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[3]")
-    puts @arrival.first.text.split[1]
-    @trains[[i,2]] = @arrival.first.text.split[1]
-    @duration = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[4]")
-    hours = @duration.first.text.split[1]
-    mins = @duration.first.text.split[3]
-    ttime = hours.to_i*60 + mins.to_i
-    puts ttime
-    @trains[[i,3]]= ttime
-  end
+     @tname = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[1]/a")
+     puts @tname.first.text
+     @trains[[i,0]]= @tname.first.text
+     @departure = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[2]")
+     puts @departure.first.text.split[1]
+     @trains[[i,1]]= @departure.first.text.split[1]
+     @arrival = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[3]")
+     puts @arrival.first.text.split[1]
+     @trains[[i,2]] = @arrival.first.text.split[1]
+     @duration = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[#{i}]/li[4]")
+     hours = @duration.first.text.split[1]
+     mins = @duration.first.text.split[3]
+     ttime = hours.to_i*60 + mins.to_i
+     puts ttime
+     @trains[[i,3]]= ttime
+   end
 
-  @allTrains if @allTrains.length > 0
-}
 
 #Writes in the DB
 journeys = session[:journeys]
@@ -94,8 +87,6 @@ puts "Total trains #{@allTrains.length}"
 puts "Total journeys in DB #{journeys.find.count}"
 
 
-#@driver.navigate.back
-end
 end 
 end  
 
