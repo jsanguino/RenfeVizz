45728bc037ac4e6bf48720e933f34675df98e523
Victoriano <victoriano@gmail.com>
2013-06-30 14:31:50 +0200

    It works fine now and control exceptions during the execution

--- mobiRenfe.rb
+++ mobiRenfe.rb
@@ -1,18 +1,24 @@
 require "selenium-webdriver"
 require 'moped'
 
-#Connect with DB
+# ----------------------------------------------------------------
+# Webdriver Settings for Renfe Scrapper - by Victoriano Izquierdo
+#-----------------------------------------------------------------
+
+#Connect with MongoDB
 session = Moped::Session.new([ "127.0.0.1:27017" ])
 session.use "renfe_vizz"
-#Writes to journeys collection in the DB
+#Writes to journeys collection in the MongoDB
 journeys = session[:journeys]
 
 @driver = Selenium::WebDriver.for :firefox
 @base_url = "http://renfe.mobi"
-#@accept_next_alert = true
 @driver.manage.timeouts.implicit_wait = 5
 
-#@verification_errors = []
+
+# ----------------------------------------------
+# Retrieve List of all cities train stations 
+#-----------------------------------------------
 
 @driver.get(@base_url + "/renfev2/busca_trenes.do")
 
@@ -23,9 +29,8 @@ for i in (1).upto(all_cities.length-1)
   all_cities[i-1] = all_cities[i].text
 end
 
-@counter = 0;
-
 #Check sanity of the new array
+@counter = 0
 for i in (0).upto(all_cities.length-2)
   for j in (0).upto(all_cities.length-2)
 
@@ -38,35 +43,49 @@ for i in (0).upto(all_cities.length-2)
   end
 end
 
+# ------------------------------------------------
+# Utility Methods for interaction with renfe.mobi
+#-------------------------------------------------
 
+# Returns a webpage with a query for a journey between two cities
 def queryJourney(o, d)
 
   @driver.get(@base_url + "/renfev2/busca_trenes.do")
 
-    # waiter for the result
-    wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+  # waiter for the result
+  wait = Selenium::WebDriver::Wait.new(:timeout => 30)
 
-    #Check if the "Buscar" button is displayed before filling the form
-    #wait.until { 
-     #button = @driver.find_elements(:xpath, ".//*[@id='details']/form/input[2]")
-     #puts "Existe boton busqueda #{button.first.text}"
-     #button if button.first.displayed?
-   #}
- 
    @driver.find_element(:name, "o").send_keys o
    @driver.find_element(:name, "d").send_keys d
-   @driver.find_element(:name, "DF").send_keys "11"
+   @driver.find_element(:name, "DF").send_keys "8"
    @driver.find_element(:name, "MF").send_keys "Julio"
    @driver.find_element(:name, "AF").send_keys "2013"
-
-   #TODO check if o and d have been selected 
+ 
    wait.until { @driver.find_element(:name, "d").displayed?}
 
-   @driver.find_element(:name, "horario").click 
+   @driver.find_element(:name, "horario").click
 
  end
 
-#Iterate through all the combination of Cities
+#Returns true for pages with no trains between a valid combinations of cities
+def noTrains?
+  return @driver.find_elements(:xpath, ".//*[@id='details']/p/a").size()>0
+end
+
+#Returns true for pages with trains between cities
+def pageWithResults?
+  return @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[*]/li[1]/a").any?
+end
+
+# Returns true for error pages
+def errorPage?
+  @driver.find_elements(:xpath, ".//*[@id='resultados']/ul").size()==0 and @driver.find_elements(:xpath, ".//*[@id='details']/p").first.text=="No ha introducido ciudad de origen o destino"
+end
+
+
+# ----------------------------------------------
+# Iterate through all the combination of Cities
+#-----------------------------------------------
 
 for i in (0).upto(all_cities.length-2)
   for j in (0).upto(all_cities.length-2)
@@ -74,47 +93,42 @@ for i in (0).upto(all_cities.length-2)
     o = all_cities[i]
     d = all_cities[j]
 
+    puts "....................>"
     puts "Departure is: " + o
     puts "Arrival is: " + d
 
-    puts "Vamos a refrescar pagina con I: #{i} J: #{j}"
     queryJourney(o, d)
-    puts "Refrescada con I: #{i} J: #{j}"
- 
-  #Journey possible combination no result
-  noOandDcombi = @driver.find_elements(:xpath, ".//*[@id='details']/p/a").size()>0
+    puts "//// queried with I: #{i} J: #{j}"
 
-  if noOandDcombi
-    puts @driver.find_elements(:xpath, ".//*[@id='details']/p/a").size()
-    puts "No trayecto I: #{i} J: #{j}"  
+  # ** Case no trains for these cities **
+  if noTrains?
+    puts " --- No trayecto para I: #{i} J: #{j} ---"  
     next
   end
 
-   # Journey with results
-  yesOandD = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul[*]/li[1]/a").any?
-  #Journey error: Empty page, requery - fallback to recoursively reload search page
-  noOnoD = @driver.find_elements(:xpath, ".//*[@id='resultados']/ul").size()==0 and @driver.find_elements(:xpath, ".//*[@id='details']/p").first.text=="No ha introducido ciudad de origen o destino"
-  
-  if noOnoD
+  # ** Case error page with no cities ** 
+  # when Firefox windows is not active 
+  if errorPage?
 
-    puts @driver.find_elements(:xpath, ".//*[@id='details']/p").first.text
-    puts "Web vacia info: #{i} J: #{j}"  
+    puts " --- Web vacia with no cities passed for: #{i} J: #{j} --- "  
     
-    #Query the journey until it does not get a non-valid page
-    #TODO break loop when get a valid page
-    until yesOandD or noOandDcombi
+    #query the journey until it does not get a non-valid page,
+    while errorPage? 
+
      queryJourney(o, d)
-    end
+     puts "**** re-queried with I: #{i} J: #{j}"
+   end
 
-   #back in a page with either results or non-journey combination
-   puts "Am I back?" 
+   puts "Web with results or no combination shown now" 
 
-    #Journey does exist? No journey, continue iterating to next city
-    if noOandDcombi
+    #Notrains? keep iterating to next cities
+    if noTrains?
      next
    end
 
-  end
+ end
+
+ # ** Case is combination of cities joined by trains ** 
 
     # wait for the results
     wait = Selenium::WebDriver::Wait.new(:timeout => 30)
